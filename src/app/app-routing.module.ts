bde5b3a1097f9bf33f093a1ea3aad6c705282fdd
andres06-hub <andressierrarojas7@gmail.com>
2023-02-01 04:20:18 -0500

    ADD: home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'medicines', pathMatch: 'full'},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
   { path: 'medicines', loadChildren: () => import('./modules/medication-table/medication-table.module').then(m => m.MedicationTableModule) },
   { path: 'sales', loadChildren: ()=> import('./modules/sales-table/sales-table.module').then(m => m.SalesTableModule) },
   { path: 'not-found', loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule) },
