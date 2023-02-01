31ee32e7f437547c8fa2528c6d5baa12a0f3ccd0
andres06-hub <andressierrarojas7@gmail.com>
2023-02-01 01:31:10 -0500

    ADD: routing of modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'medicines', pathMatch: 'full'},
+  {path: 'medicines', loadChildren: () => import('./modules/medication-table/medication-table.module').then(m => m.MedicationTableModule)},
+  {path: 'not-found', loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule)},
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
