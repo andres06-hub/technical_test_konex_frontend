a49414a1defc21e45607b908f03c29d478e4f706
andres06-hub <andressierrarojas7@gmail.com>
2023-02-01 02:37:23 -0500

    ADD: Module Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'medicines', pathMatch: 'full'},
-  {path: 'medicines', loadChildren: () => import('./modules/medication-table/medication-table.module').then(m => m.MedicationTableModule)},
-  {path: 'not-found', loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule)},
-  {path: '**', redirectTo: 'not-found', pathMatch: 'full'},
+  { path: '', redirectTo: 'medicines', pathMatch: 'full'},
+  { path: 'medicines', loadChildren: () => import('./modules/medication-table/medication-table.module').then(m => m.MedicationTableModule) },
+  { path: 'sales', loadChildren: ()=> import('./modules/sales-table/sales-table.module').then(m => m.SalesTableModule) },
+  { path: 'not-found', loadChildren: () => import('./modules/not-found/not-found.module').then(m => m.NotFoundModule) },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
