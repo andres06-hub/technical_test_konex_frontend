c024191f26791d8b9b6b1cdb53da2a726079d851
andres06-hub <andressierrarojas7@gmail.com>
2023-02-05 00:58:49 -0500

    MODOFIED

--- src/app/modules/medication-table/service/medicines.service.ts
+++ src/app/modules/medication-table/service/medicines.service.ts
@@ -16,24 +16,4 @@ export class MedicinesService {
   getAllMedicines() {
     return this._http.get<ApiResponse<Medicine[]>>(API_HOST+'medicines');
   }
-
-  getMedicineFilter() {
-    return this._http.get(API_HOST+'medicines/filter');
-  }
-
-  //Vender el medicamento con la cantidad
-  saleMedical(obj: Medicine, quantity: number) {
-    console.warn("SALE ", new Date().getTime());
-    return this._http.post(API_HOST+'sale', {
-      "publicIdMedicine": obj.publicId,
-      "saleDateTime": new Date().getTime(),
-      "quantity": quantity
-    },
-    {
-      headers: new HttpHeaders({
-        "Content-Type": "application/json"
-      }),
-    }
-    );
-  }
 }
