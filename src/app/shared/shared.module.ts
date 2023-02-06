a8b38801801ef74391d2f0246ccd823dd646ec44
andres06-hub <andressierrarojas7@gmail.com>
2023-02-06 00:35:46 -0500

    ADD: services and modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, FormStyle } from '@angular/common';
 import { TableComponent } from './components/table/table.component';
 import { TableModule } from 'primeng/table';
 import { CartComponent } from './components/cart/cart.component';
@@ -7,6 +7,9 @@ import { RouterModule } from '@angular/router';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { MedicineTableService } from '../common/services/medicine-table.service';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ConfirmationService } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,17 +18,20 @@ import { MedicineTableService } from '../common/services/medicine-table.service'
   ],
   imports: [
     CommonModule,
+    FormsModule,
     TableModule,
     InputTextModule,
     ButtonModule,
     RouterModule,
+    ConfirmPopupModule,
   ],
   exports: [
     TableComponent,
     CartComponent
   ],
   providers: [
-    MedicineTableService
+    MedicineTableService,
+    ConfirmationService,
   ]
 })
 export class SharedModule { }
